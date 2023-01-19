0891e388a35d9034bf8ba2525ca589655a0a2b36
xanderlambert <xanderlambert@gmail.com>
2023-01-19 21:59:42 +0000

    letsGetFunctional_Day1

--- projects/let-s-get-functional/index.js
+++ projects/let-s-get-functional/index.js
@@ -3,7 +3,7 @@
 'use strict';
 
 var customers = require('./data/customers.json');
-var _ = require(/* Replace this with the name of your lodown! */);
+var _ = require('underbar');
 
 /**
  * 1. Import your lodown module using the require() method,
@@ -16,26 +16,85 @@ var _ = require(/* Replace this with the name of your lodown! */);
  *
  * 4. To test your work, run the following command in your terminal:
  *
- *    npm start --prefix ./<YOUR_GITHUB_FOLDER/projects/let-s-get-functional
- *
+ * ************For Testing************   npm start --prefix ./alex-lambert.github.io/projects/let-s-get-functional
+ * 
  *    IMPORTANT: Make sure you replace <YOUR_GITHUB_FOLDER with your actual github folder name that is in your workspace.
  */
 
 var maleCount = function(array) {
 
+    let males = _.filter(array, function(customer) {
+        return customer.gender === 'male';
+    });
+
+    return males.length;
+
 };
 
-var femaleCount;
+var femaleCount = function(array) {
+    let females = _.reduce(array, function(prev, current) {
+            if (current.gender === 'female') {
+                prev += 1;
+            }
+            return prev;
+    }, 0);
+    return females;
+};
 
-var oldestCustomer;
+var oldestCustomer = function(array) {
+    let old = _.reduce(array, function(acc, current) {
+        if (acc.age < current.age) {
+            acc = current;
+        } 
+        return acc;
+    });
+    return old.name;
+};
 
-var youngestCustomer;
 
-var averageBalance;
+//use reduce method w/no seed value. accumulate which customer is yougest oldest
+var youngestCustomer = function(array) {
+    let young = _.reduce(array, function(acc, current) {
+        if (acc.age > current.age) {
+            acc = current;
+        } 
+        return acc;
+    });
+    return young.name;
+};
 
-var firstLetterCount;
+var averageBalance = function(array) {
+    let total = _.reduce(array, function(acc, current) {
+        let x = current.balance.replace(/[$,]/g,'');
+        acc += Number(x);
+        return acc;              
+    }, 0);
+    return total/array.length;
+};
 
-var friendFirstLetterCount;
+var firstLetterCount = function(array, char) {
+    let total = _.reduce(array, function(acc, current) {
+            if (current.name[0].toUpperCase() === char.toUpperCase()) {
+                acc += 1;
+            }
+        return acc;
+    }, 0);
+    return total;
+};
+
+var friendFirstLetterCount = function(array, customer, char) {
+    let count = 0;
+    for ( let i =0; i < array.length; i++) {
+        if (array[i].name === customer) {
+            for (let ii = 0; ii < array[i].friends[ii].name[0]; i++) {
+                if (array[i].friends[ii].name[0] === char) {
+                    count++;
+                }
+            }
+        }
+    }
+    return count;
+};
 
 var friendsCount;
 
--- studies/operators.js
+++ studies/operators.js
@@ -4,7 +4,7 @@
  Operators are used to assign & compare values, perform arithmetic, or apply logic. * */ 
 
 //1.Assignment Operators (= += -= *= /= %= **= )
-//The Assignment Operators assign a value to OR update the value of a variable based on the first arithmetic symbol of the operator.
+//Assignment Operators assign a value to OR update the value of a variable based on the first arithmetic symbol of the operator.
 let num = 2; //assigning num to 2;
 num += 3; //adds the two sides and sets num equal to result
 console.log(num); //=> logs 5
@@ -14,7 +14,7 @@ num = 1; ////reassigning num to 1;
 console.log(num); //=> logs 1
 
 //2.Arithmetic Operators (+ - * / % **)
-//The Arithmetic Operators perform arithmetic on a value.
+//Arithmetic Operators perform arithmetic on a value.
 let val = 2 + 2; //addition operator
 console.log(val); //=> logs 4
 let val2 = val * 10; //multiplication operator
@@ -22,13 +22,27 @@ console.log(val2); //=> logs 40
 let val3 = val2 % 4; //remainder/modulo operator
 console.log(val3); //=> logs 0(remainder)
 
-//3.Comparison Operators (> < >= <= === ! )
-//The Comparison Operators assign a value to OR update the value of a variable.
-// let num = 2;
-// num += 3;
-// console.log(num); //=> logs 5
-// num *= 10;
-// console.log(num); //=> logs 50
-// num = 1;
-// console.log(num); //=> logs 1
+//3.Comparison Operators (> < >= <= == === ! )
+//Comparison Operators are used in logical statements to determine differences (or equality) between variables/values.
+// Comparison operators return a boolean. 
+//
+console.log(1 > 10); //=> logs false
+console.log(1 < 10); //=> logs true
+let x = 0;
+if ( x >= 0) {
+  console.log('this is true');
+} else {
+  console.log('this is false');
+}//=> logs this is false
+let y = -5;
+if ( y >= 0) {
+  console.log('this is true');
+} else {
+  console.log('this is false');
+} //=> logs this is false
+console.log(num); //=> logs 5
+num *= 10;
+console.log(num); //=> logs 50
+num = 1;
+console.log(num); //=> logs 1
 
