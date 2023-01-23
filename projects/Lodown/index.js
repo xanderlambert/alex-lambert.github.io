c2bc96023936ca022717a3414edff895f13c69c9
xanderlambert <xanderlambert@gmail.com>
2023-01-23 02:30:17 +0000

    Lowdown_Day_1

--- projects/Lodown/index.js
+++ projects/Lodown/index.js
@@ -1,12 +1,19 @@
 
-
-
-
+/***
+* identity: takes an input value and returns it, unchanged.
+* 
+* @param {Any} value: The input value that will be returned as is.
+*/ 
 _.identity = function(value) {
     return value;
 }
+module.exports.identity = identity;
 
 
+/***
+* typeOf: Returns the data type of the input <value> as a string
+* @param {Any} value: The value for which type is to be determined.
+*/ 
 _.typeOf = function(value) {
     if(Array.isArray(value)) {
         return 'array';
@@ -16,7 +23,17 @@ _.typeOf = function(value) {
         return typeof value;
     }  
 }
+module.exports.typeOf = typeOf;
 
+
+/***
+* first: Designed to loop over an array and return the first <number> items of <array>.
+* If <number> is not given or not a number, returns just the first element in <array>.
+* If <array> is not an array, returns [].
+* 
+* @param {Array} array: The array over which to iterate.
+* @param {Number} number: Identifies first <number> of items in <array> to return.
+*/ 
 _.first = function(array, number) {
     if(!Array.isArray(array) || number < 0) {
         return [];
@@ -28,7 +45,17 @@ _.first = function(array, number) {
         return array.slice(0, number);
     }
 }
+module.exports.first = first;
 
+
+/***
+* last: Designed to loop over an array and return the last <number> items of <array>.
+* If <number> is not given or not a number, returns just the last element in <array>.
+* If <array> is not an array, returns []
+* 
+* @param {Array} array: The array over which to iterate.
+* @param {Number} number: Identifies last <number> of items in <array> to return.
+*/ 
 _.last = function(array, number) {
     if(!Array.isArray(array) || number < 0) {
         return [];
@@ -40,6 +67,26 @@ _.last = function(array, number) {
         return array.slice(number - 1);
     }
 }
+module.exports.last = last;
+
+
+
+/***
+* indexOf: Returns the index of <array> that is the first occurrance of <value>.
+* Returns -1 if <value> is not in <array>.
+* 
+* @param {Array} arr: The array being checked for occurences of <value>.
+* @param {Value} val: Identifies the <value> being looked for within <array>.
+*/ 
+_.indexOf = function(arr, val) {
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === val) {
+            return i;
+        } 
+    }
+    return -1;
+}
+module.exports.indexOf = indexOf;
 
 /**
  * each: Designed to loop over a collection, Array or Object, and applies the 
